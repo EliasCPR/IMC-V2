acbf52fdf934798f9127e795145da94fa06b90cf
Elias Mateus Prado Oliveira <eliascpr123@gmail.com>
2021-03-18 11:32:01 -0300

    first commit

---
+++ app/src/main/java/sp/senai/br/aula19_03/MainActivity.kt
@@ -0,0 +1,11 @@
+package sp.senai.br.aula19_03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
